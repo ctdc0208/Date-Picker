d426356fb67d2f20716497459bb688556b43b9d3
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2024-02-07 08:10:19 +0800

    add input value and option value

--- src/DateField.jsx
+++ src/DateField.jsx
@@ -3,8 +3,65 @@ import {useState} from 'react';
 import Calendar from 'react-calendar';
 
 export default function DateField() {
+    const timeOptions = [
+        {value: '09:30 AM', text: "09:30 AM"},
+        {value: '10:00 AM', text: "10:00 AM"},
+        {value: '10:30 AM', text: "10:30 AM"},
+        {value: '11:00 AM', text: "11:00 AM"},
+        {value: '11:30 AM', text: "11:30 AM"},
+        {value: '12:00 PM', text: "12:00 PM"},
+        {value: '12:30 PM', text: "12:30 PM"},
+        {value: '01:00 PM', text: "01:00 PM"},
+        {value: '01:30 PM', text: "01:30 PM"},
+        {value: '02:00 PM', text: "02:00 PM"},
+        {value: '02:30 PM', text: "02:30 PM"},
+        {value: '03:00 PM', text: "03:00 PM"},
+        {value: '03:30 PM', text: "03:30 PM"},
+        {value: '04:00 PM', text: "04:00 PM"},
+        {value: '04:30 PM', text: "04:30 PM"},
+        {value: '05:00 PM', text: "05:00 PM"},
+    ];
+
+    const timezoneOptions = [
+        {value: 'EST', text: "EST"},
+        {value: 'CST', text: "CST"},
+    ];
+
+    const monthNames = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+    ];
+
+
     const [isOpen, setIsOpen] = useState(true);
-    const [value, onChange] = useState(new Date());
+    const [calendarValue, calendarValueOnChange] = useState(new Date());
+    const [timeValue, timeValueOnChange] = useState(timeOptions[0].value);
+    const [timezoneValue, timezoneValueOnChange] = useState(timezoneOptions[0].value);
+
+    const month = calendarValue.getMonth()
+    const day = calendarValue.getDate()
+    const year = calendarValue.getFullYear();
+    const getMonthNames = monthNames[month]
+
+    const value = day + '/' + getMonthNames + '/' + year +' ' + timeValue + ' ' + '(' + timezoneValue + ')'
+
+    const handleTimeChange = event => {
+        timeValueOnChange(event.target.value);
+    };
+    const handleTimezoneChange = event => {
+        timezoneValueOnChange(event.target.value);
+    };
+    
     return (
     <>
         <button onClick={() => setIsOpen(!isOpen)} className="flex flex-row items-center mt-5 w-128 h-10 border border-solid border-gray rounded-lg ring-offset-8 ">
@@ -24,39 +81,33 @@ export default function DateField() {
             />
         </button>
         {isOpen && (
-            <div className='mt-2 border rounded-md'>
+            <div className='mt-2 rounded-md'>
                 <Calendar 
-                    onChange={onChange} 
-                    value={value}
-                    className='w-128'
+                    onChange={calendarValueOnChange} 
+                    value={calendarValue}
+                    className='w-128  border rounded-xl'
+                    prevLabel="<"
+                    nextLabel=">"
                 />
                 <div className='border-t-2 border-gray p-5 space-x-7 flex flex-row justify-center w-128'>
                     <div>
                         <div className='font-bold text-sm'>Publication Time</div>
-                        <select name='time' id='time' className='border bottom-1 border-gray p-2 mt-2 w-44 rounded-lg text-gray'>
-                            <option value="09:30 AM">09:30 AM</option>
-                            <option value="10:00 AM">10:00 AM</option>
-                            <option value="10:30 AM">10:30 AM</option>
-                            <option value="11:00 AM">11:00 AM</option>
-                            <option value="11:30 AM">11:30 AM</option>
-                            <option value="12:00 PM">12:00 PM</option>
-                            <option value="12:30 PM">12:30 PM</option>
-                            <option value="12:00 PM">01:00 PM</option>
-                            <option value="01:30 PM">01:30 PM</option>
-                            <option value="02:00 PM">02:00 PM</option>
-                            <option value="02:30 PM">02:30 PM</option>
-                            <option value="03:00 PM">03:00 PM</option>
-                            <option value="03:30 PM">03:30 PM</option>
-                            <option value="04:00 PM">04:00 PM</option>
-                            <option value="04:30 PM">04:30 PM</option>
-                            <option value="05:00 PM">05:00 PM</option>
+                        <select name='time' id='time' onChange={handleTimeChange} value={timeValue} className='border bottom-1 border-gray p-2 mt-2 w-44 rounded-lg text-gray'>
+                            {timeOptions.map(option => (
+                                <option key={option.value} value={option.value}>
+                                    {option.text}
+                                </option>
+                            ))};
                         </select>
                     </div>
                     <div>
                         <div className='font-bold text-sm'>Publication Timezone</div>
-                        <select name='timezone' id='timezone' className='border bottom-1 border-gray p-2 mt-2 w-44 rounded-lg text-gray'>
-                            <option value="EST">EST</option>
-                            <option value="CST">CST</option>
+                        <select name='timezone' id='timezone' onChange={handleTimezoneChange} value={timezoneValue} className='border bottom-1 border-gray p-2 mt-2 w-44 rounded-lg text-gray'>
+                            {timezoneOptions.map(option => (
+                                <option key={option.value} value={option.value}>
+                                    {option.text}
+                                </option>
+                            ))};
                         </select>
                     </div>
                 </div>
